34527740555f8127999cc769bce41ee7eeafb720
Aoi Irie <eai@mizle.net>
2019-12-21 09:55:01 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+<h1 align="center">
+<img src="https://cdnjs.cloudflare.com/ajax/libs/twemoji/12.0.4/2/svg/1f468-200d-1f373.svg" width="150px">
+
+Soup Chef
+</h1>
+
+[![Invite this Bot](https://img.shields.io/badge/Discord-Invite%20this%20bot-green?logo=discord&style=flat-square)](https://discordapp.com/oauth2/authorize?&client_id=657748609323892757&scope=bot)
+
+
+## Self-hosted Usage
+
+```bash
+cp .env.sample .env
+# https://discordapp.com/developers/applications/me
+# でアプリケーションを作成して、アプリケーションの中でbotを作成し、botのtokenを
+# .env の DISCORD_TOKEN に入れる
+
+# https://discordapp.com/oauth2/authorize?&client_id=アプリケーションのClient ID&scope=bot
+# を開いてbotをサーバーに招待する
+
+yarn
+yarn start
+```
+
+
+## Licence
+
+This project is licensed under the MIT License.
---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import { Client, TextChannel } from "discord.js";
+
+require("dotenv").config();
+
+const client = new Client();
+const soupReactions = process.env.SOUP_REACTIONS.split(",");
+
+client.on("ready", () => {
+    console.log("I am ready!");
+    client.user.setActivity("github.com/eai04191/discord-soup-bot");
+});
+
+// どこかのチャンネルにメッセージが投稿されたら
+client.on("message", message => {
+    // 投稿されたのがテキストチャンネル かつ
+    // チャンネル名がスープチャンネル かつ
+    // メッセージの末尾が"?"か"？" なら
+    if (
+        message.channel instanceof TextChannel &&
+        message.channel.name === process.env.SOUP_CHANNEL_NAME &&
+        /\?|？$/.test(message.content)
+    ) {
+        console.log(`Question Posted: ${message.content}`);
+        // 順番にリアクションをつける
+        soupReactions.reduce(
+            (promise, emoji) => promise.then(() => message.react(emoji)),
+            Promise.resolve()
+        );
+    }
+});
+
+// どこかのチャンネルのメッセージにリアクションが追加されたら
+// 注: キャッシュされている（=bot起動後に投稿された）メッセージにしか反応しない
+client.on("messageReactionAdd", (messageReaction, user) => {
+    // リアクションがついたメッセージが投稿されたのがテキストチャンネル かつ
+    // リアクションがついたメッセージがチャンネル名がスープチャンネル かつ
+    // リアクションしたのがこのbotではない かつ
+    // 追加されたリアクションがスープのどれか なら
+    if (
+        messageReaction.message.channel instanceof TextChannel &&
+        messageReaction.message.channel.name ===
+            process.env.SOUP_CHANNEL_NAME &&
+        user !== client.user &&
+        soupReactions.includes(messageReaction.emoji.name)
+    ) {
+        // 自分のリアクションをすべて消す
+        messageReaction.message.reactions.forEach(reaction => {
+            reaction.remove(client.user);
+        });
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
